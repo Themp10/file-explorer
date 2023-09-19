f9c9a2eaa03a6a40afffd46676fc43b3735503a8
Themp10 <oussamaaboujaafar@gmail.com>
2023-09-19 18:47:43 +0000

    j

--- api/ftp.router.js
+++ api/ftp.router.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const ftp = require('basic-ftp');
 const multer = require('multer')
-//;:
 // Create an FTP client
 const client = new ftp.Client();
 
